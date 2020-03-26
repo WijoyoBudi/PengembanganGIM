5f0b908497f1232217c5755764ba4313b97e4551
WijoyoBudi <18523191@students.uii.ac.id>
2020-03-26 15:15:15 +0700

    Penambahan Waktu

--- Assets/sc.cs
+++ Assets/sc.cs
@@ -6,9 +6,10 @@ public class sc : MonoBehaviour
 {
      // Start is called before the first frame update
     public int speed = 30;
+    public Rigidbody2D sesuatu;
     void Start()
     {
-        GetComponent<Rigidbody2D>().velocity = new Vector2(-1,1)*speed;
+        sesuatu.velocity = new Vector2(-1,1)*speed;
     }
 
     // Update is called once per frame
